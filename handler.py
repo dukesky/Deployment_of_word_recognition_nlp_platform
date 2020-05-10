a71b78043ab7c9cfe1d48a8da8633123cc73458c
dukesky <36315007+dukesky@users.noreply.github.com>
2020-05-09 21:24:21 -0700

    update handler and serverless files

--- handler.py
+++ handler.py
@@ -1,15 +1,52 @@
+print('container start')
+
+try:
+    import unzip_requirements
+except ImportError as error:
+    pass
+
 import json
 
+import spacy
+import en_core_web_sm
 
-def hello(event, context):
+MODEL = en_core_web_sm.load()
+print('model loaded')
+
+def create_ner_spans(text):
+
+    doc = MODEL(text)
+    spans = []
+    for ent in doc.ents:
+        span = {
+            'start': ent.start_char,
+            'end': ent.end_char,
+            'type': ent.label_,
+        }
+    return spans
+
+
+def handle_request(event, context):
+    text = event['body']
+    print('received test from http post: ',text)
+    spans = []
+
+    if text is not None:
+        spans = create_ner_spans(text)
+    print('spans after create: ',spans)
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+       "spans": spans,
+
     }
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body),
+        "headers": {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
     }
 
     return response
